9a64d21a940d58ed6ca209b7e77ec2380bc7524f
dskrenta <dskrenta@gmail.com>
2016-09-11 02:28:42 -0700

    stuff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-// app.use(express.static('src'));
+app.use(express.static('src'));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
